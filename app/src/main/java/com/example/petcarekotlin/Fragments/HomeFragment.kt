bf8d0c746c73aece1b36bba314d71d5d6dbd61d9
Jinal Patel <jinal.patel@gtcsys.com>
2023-03-07 14:43:22 +0530

    setup

--- app/src/main/java/com/example/petcarekotlin/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/petcarekotlin/Fragments/HomeFragment.kt
@@ -12,7 +12,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
